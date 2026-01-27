cce70de7f885f8f65add635eff42a14b832b0dd3
xhnan <hain.xiao@gmail.com>
2026-01-27 11:01:51 +0800

    添加存储键名常量

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+/**
+ * 存储键名常量
+ */
+export const STORAGE_KEYS = {
+    TOKEN: 'life_hub_token',
+    TOKEN_EXPIRES_AT: 'life_hub_tokenExpiresAt',
+    USER_INFO: 'life_hub_userInfo',
+    USER_ROLES: 'life_hub_userRoles',
+    USER_PERMISSIONS: 'life_hub_userPermissions',
+    MENU_DATA: 'life_hub_menuData'
+} as const;
